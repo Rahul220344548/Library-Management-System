983ea22103b6288ef64d795a08ed1eefba1c5a69
Rahul Vasantlal <220344548@aston.ac.uk>
2024-08-28 14:39:47 +0100

    add constuctor and attributes to book class

---
+++ LibraryBook/src/Book.java
@@ -0,0 +1,25 @@
+
+public class Book {
+	
+	private int bookID;
+	private String title;
+	private String author;
+	private boolean isIssued;
+	
+	public Book( int bookID, String title, String author, boolean isIssued ) {
+		this.bookID = bookID;
+		this.title = title;
+		this.author = author;
+		this.isIssued = isIssued;
+	}
+	
+	public int getBookID() {
+		return bookID;
+	}
+	
+	
+	
+	
+	
+	
+}
--- LibraryBook/src/main.java
+++ LibraryBook/src/main.java
@@ -6,8 +6,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		main myObj = new main();
-		System.out.println(myObj.name);
+		Book myBook = new Book(1,"Atomic Habits","Rahul Vasantlal", true);
+		
 		
 		
 
