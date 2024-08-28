fa4c62c1f5dfc13540880edacce5723a66182f23
Rahul Vasantlal <220344548@aston.ac.uk>
2024-08-28 21:33:18 +0100

    add code to user class to intiiate user objects

--- LibraryBook/src/User.java
+++ LibraryBook/src/User.java
@@ -1,4 +1,24 @@
 
 public class User {
-
+	
+	private int userID;
+	private String name;
+	private boolean isIssued;
+	
+	public User(int userID, String name,boolean isIssued) {
+		this.userID = userID;
+		this.name = name;
+		this.isIssued = isIssued;
+	}
+	
+	public int getUserID() {
+		return userID;
+	}
+	public String getName() {
+		return name;
+	}
+	public boolean checkisIssued() {
+		return isIssued;
+	}
+	
 }
