7cb551bbc5e56d00c84a57f283d03f106c47ed99
Rahul Vasantlal <220344548@aston.ac.uk>
2024-08-29 19:00:41 +0100

    add code to read csv file externally and add function to get current data

--- LibraryBook/src/Library.java
+++ LibraryBook/src/Library.java
@@ -1,22 +1,53 @@
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class Library {
 	
-	Map<Integer, User> bookData = new HashMap<>();
-//	Map<Integer, Student> userData = new HashMap<>();
+	Scanner input = new Scanner(System.in);
+	Map<Integer, Book> bookData = new HashMap<>();
+	Map<Integer, User> userData = new HashMap<>();
+	private static int userIDCounter = 1;
+	
+	public Library() {
+        userData = new HashMap<>();
+    }
 	
 	
-	private void addNewBook() {
-		bookData.put(1, new User(1,"Rahul",false));
+	public void addNewBook() {
+		String name;
+		boolean isIssued = false;
+//		User newUser = new User();
+		System.out.print("Enter a name: ");
+		name = input.next();
+		userData.put(1, new User(1,name,false));
+		
+		System.out.print(userData.get(1));
 	}
 	
 	private void removeBook(/*Book*/) {
 		
 	}
 	
-	private void addUser(/*User*/) {
+	public void addUser(/*User*/) {
 		
+        System.out.print("Enter name: ");
+        String name = input.next();
+        
+        System.out.println("Enter address: ");
+        String address = input.next();
+        
+        
+        
+//		userData.put(1, new User(1,"Rahul",false));
+//
+//		readUsersFile();
+	
 	}
 	private void issueBook(/*bookID, userID*/) {
 		
@@ -33,5 +64,41 @@ public class Library {
 	
 	private void displayUsers() {
 		
+	}
+	
+	private void readUsersFile() {
+		String file = "C:\\Users\\rahul\\git\\repository\\LibraryBook\\src\\Users.csv";
+		BufferedReader reader = null;
+		String line = "";
+		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			while((line = reader.readLine()) != null ) {
+				String [] row = line.split(",");
+				
+				for ( String index : row ) {
+					System.out.printf("%30s", index);
+				}
+				System.out.println();
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private String getCurrentDate() {
+		LocalDateTime currentDateTime = LocalDateTime.now();
+        // Define the formatter for dd/MM/yyyy HH:mm:ss
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        
+        return formattedDateTime;
 	}
 }
