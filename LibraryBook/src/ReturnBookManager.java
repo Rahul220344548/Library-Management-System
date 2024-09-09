12a5603c26ebf4259f579f5d0555fa23d49431c6
Rahul Vasantlal <220344548@aston.ac.uk>
2024-09-09 15:54:05 +0100

    create a new class for return book methods

---
+++ LibraryBook/src/ReturnBookManager.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReturnBookManager {
+	
+	private String filePath;
+	
+	public ReturnBookManager (String filePath) {
+        this.filePath = filePath;
+    }
+	
+	
+	
+
+}
